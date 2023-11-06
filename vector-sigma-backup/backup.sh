52318115204bcc2bd3277f45386dcb71ced9a285
formula-spectre <purity-piped@tuta.io>
2023-11-06 14:10:54 +0100

    autosync from vector-sigma @ Mon 06 Nov 2023 02:10:54 PM CET

--- vector-sigma-backup/backup.sh
+++ vector-sigma-backup/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 [ -d ~/ww/vector-sigma-backup ] || mkdir -p ~/ww/vector-sigma-backup
-cp -a /etc/fstab ~/ww/vector-sigma-backup/fstab
-cp -a ~/backup.sh ~/ww/vector-sigma-backup/backup.sh
+cp  -- /etc/fstab ~/ww/vector-sigma-backup/fstab
+cp -- ~/backup.sh ~/ww/vector-sigma-backup/backup.sh
 eval $(ssh-agent)
 ssh-add ~/.ssh/id_ed25519-gh
 
